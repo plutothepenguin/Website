c09c472beb061d02367ce4d3ccebcda8be34b8b4
omidghafori <cheeesebrain@gmail.com>
2019-05-09 21:08:19 -0400

    added scroll functionality, changed bg color of 2nd section, changed font

--- javascript.js
+++ javascript.js
@@ -3,6 +3,8 @@ $('.outerCircle').mouseover(function() {
   $('.outerCircle').css('height', '505px');
   $('.innerCircle').css('width', '455px');
   $('.innerCircle').css('height', '455px');
+  $('.title').css('font-size', '72px');
+  $('.outerCircle').css('box-shadow', '0px 0px 20px 10px #fff');
   // $('.innerCircle').css('background-color', 'white');
 })
 $('.outerCircle').mouseout(function() {
@@ -10,15 +12,37 @@ $('.outerCircle').mouseout(function() {
   $('.outerCircle').css('height', '500px');
   $('.innerCircle').css('width', '450px');
   $('.innerCircle').css('height', '450px');
+  $('.title').css('font-size', '69px');
+  $('.outerCircle').css('box-shadow', '0px 0px 5px 3px #fff');
   // $('.innerCircle').css('background-color', '#eeeeee');
 })
-let scroll;
-$(window).scroll(function (event) {
-    scroll = $(window).scrollTop();
-    if (scroll >= $(window).height()) {
-      // alert('yes');
-    }
-    scroll = (100-((((scroll - 0) * (100 - 0)) / ($(window).height() - 0)) + 0))/100;
-    $('.top').css('opacity', scroll);
+$('.outerCircle').mousedown(function() {
+  $('.outerCircle').css('width', '500px');
+  $('.outerCircle').css('height', '500px');
+  $('.innerCircle').css('width', '450px');
+  $('.innerCircle').css('height', '450px');
+  $('.title').css('font-size', '69px');
+  $('.outerCircle').css('box-shadow', '0px 0px 0px 0px #fff');
 
-});
+  // $(window).scrollTop($(window).height());
+})
+$('.outerCircle').mouseup(function() {
+  $('.outerCircle').css('width', '505px');
+  $('.outerCircle').css('height', '505px');
+  $('.innerCircle').css('width', '455px');
+  $('.innerCircle').css('height', '455px');
+  $('.title').css('font-size', '72px');
+  $('.outerCircle').css('box-shadow', '0px 0px 20px 10px #fff');
+  $('html').animate({scrollTop: $(window).height()}, 500);
+})
+let scroll;
+// $(window).scroll(function (event) {
+//     scroll = $(window).scrollTop();
+//     if (scroll >= $(window).height()) {
+//       // alert('yes');
+//     }
+//     scroll = ((((scroll - 0) * (100 - 0)) / ($(window).height() - 0)) + 0);
+//     $('.top').css('opacity', (100-scroll)/100);
+//     $('.middle').css('opacity', (200-scroll)/100)
+//
+// });
